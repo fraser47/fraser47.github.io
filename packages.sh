9bee01fd9c3e4215b6ab195bcbdb8fd816788725
NsBm0d <NsBm0d@live.com>
2016-08-02 12:37:08 +1000

    init

---
+++ packages.sh
@@ -0,0 +1,12 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf470
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\froman\fcharset0 Times-Roman;}
+{\colortbl;\red255\green255\blue255;\red108\green108\blue108;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl360\sa240\partightenfactor0
+
+\f0\fs32 \cf2 \expnd0\expndtw0\kerning0
+#!/bin/bash\uc0\u8232 dpkg-deb -bZgzip projects/Nameofpackage debs\
+                                                                              
+\f1\fs24 \cf0 \
+}
