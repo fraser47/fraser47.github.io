966158599ea75ca514133417ffe0e2c1da054b0a
NsBm0d <NsBm0d@live.com>
2016-08-02 12:49:45 +1000

    init

--- push.sh
+++ push.sh
@@ -1,10 +1,3 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf470
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;\red108\green108\blue108;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl360\sa240\partightenfactor0
-
-\f0\fs32 \cf2 \expnd0\expndtw0\kerning0
-#!/bin/bash\uc0\u8232 dpkg-scanpackages ./debs > Packages bzip2 -fks Packages\
-                                                                                             }
+#!/bin/bash
+dpkg-scanpackages ./debs > Packages bzip2 -fks Packages
+#make sure to leave a blank line at the end                                                                                           }
--- remove.sh
+++ remove.sh
@@ -1,10 +1,1 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf470
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;\red108\green108\blue108;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl360\sa240\partightenfactor0
-
-\f0\fs32 \cf2 \expnd0\expndtw0\kerning0
-#!/bin/bash\uc0\u8232 find debs -type f -name '*.deb' -delete rm -r Packages.bz2\
-                                                                                                  }
+#!/bin/bashfind debs -type f -name '*.deb' -delete rm -r Packages.bz2#make sure to leave a blank line at the end
